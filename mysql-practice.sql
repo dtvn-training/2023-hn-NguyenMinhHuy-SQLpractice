85ff7aff0fa431403626f0b12d550f032bf90f8a
Nguyen Minh Huy <huynguyenminh.1106@gmail.com>
2023-11-15 10:26:45 +0700

    update index

--- mysql-practice.sql
+++ mysql-practice.sql
@@ -42,7 +42,7 @@ ADD CONSTRAINT fk_product_id
 FOREIGN KEY (product_ordered)
 REFERENCES products(product_id);
 
--- Create 10 users
+-- Create users
 INSERT INTO users (user_id, first_name, last_name, address, email)
 VALUES
 (1, 'John', 'Doe', '123 Main St', 'john.doe@ex.com'),
@@ -54,9 +54,13 @@ VALUES
 (7, 'Olivia', 'Davis', '404 Elm St', 'ol.davis@ex.com'),
 (8, 'Max', 'Moore', '505 Walnut St', 'max.moore@ex.com'),
 (9, 'Sophia', 'Smith', '606 Oak St', 'sophia.smith@ex.com'),
-(10, 'Liam', 'Jones', '707 Pine St', 'liam.jones@ex.com');
+(10, 'Liam', 'Jones', '707 Pine St', 'liam.jones@ex.com'),
+(11, 'John', 'Terry', '13 Lain St', 'john.ter@ex.com'),
+(12, 'Olivia', 'Rodrigo', '404 Err St', 'ol.rod@ex.com'),
+(13, 'Eva', 'Cudning', '200 Cedric St', 'eva.cudning@ex.com'),
+(14, 'Liam', 'Payne', '40 Pile St', 'liam.payne@ex.com');
 
--- Create 10 products
+-- Create products
 INSERT INTO products (product_id, product_name, description, price)
 VALUES
 (1, 'Product A', 'Description for Product A', 19.99),
@@ -70,7 +74,7 @@ VALUES
 (9, 'Product I', 'Description for Product I', 99.99),
 (10, 'Product J', 'Description for Product J', 109.99);
 
--- Create 20 orders
+-- Create orders
 INSERT INTO orders (order_id, user_id, product_ordered, total_paid)
 VALUES
 (1, 1, 1, 19.99),
@@ -92,7 +96,9 @@ VALUES
 (17, 7, 9, 99.99),
 (18, 8, 10, 109.99),
 (19, 9, 1, 19.99),
-(20, 10, 2, 29.99);
+(20, 10, 2, 29.99),
+(21, 1, 1, 30);
+
 
 CREATE VIEW test_view AS 
 SELECT order_id, user_id
@@ -102,6 +108,10 @@ WHERE order_id > 5;
 -- Create index, explain query
 CREATE INDEX idx_user_id ON users (user_id);
 
+ALTER TABLE orders
+ADD INDEX idx_order_id_hash (`order_id`) USING HASH;
+-- Not support Hash index
+
 EXPLAIN 
 SELECT u.user_id, u.first_name, u.last_name, o.order_id, o.product_ordered 
 FROM users u 
@@ -109,6 +119,7 @@ JOIN orders o ON u.user_id = o.user_id
 WHERE u.user_id > 4;
 
 -- select users that have at least 2 orders and have order id > 4
+EXPLAIN ANALYZE
 SELECT u.user_id, u.first_name, u.last_name, COUNT(o.order_id) AS "number of orders"
 FROM users u
 JOIN orders o ON u.user_id = o.user_id
@@ -123,3 +134,53 @@ SAVEPOINT sp;
 DELETE FROM products WHERE product_id = 11;
 ROLLBACK TO SAVEPOINT sp;
 COMMIT;
+
+-- Isolating index
+EXPLAIN ANALYZE
+SELECT user_id FROM users
+WHERE user_id + 5 > 10;
+
+EXPLAIN ANALYZE
+SELECT user_id FROM users
+WHERE user_id > 5;
+
+-- Index Selectivity
+SELECT COUNT(DISTINCT first_name)/COUNT(*) FROM users;
+
+SELECT COUNT(DISTINCT LEFT(first_name, 1))/COUNT(*) AS sel1,
+COUNT(DISTINCT LEFT(first_name, 2))/COUNT(*) AS sel2,
+COUNT(DISTINCT LEFT(first_name, 3))/COUNT(*) AS sel3,
+COUNT(DISTINCT LEFT(first_name, 4))/COUNT(*) AS sel4,
+COUNT(DISTINCT LEFT(first_name, 5))/COUNT(*) AS sel5
+FROM users;
+
+-- Prefix Index
+CREATE INDEX idx_first_name ON users (first_name(2));
+
+EXPLAIN
+SELECT first_name, last_name, address FROM users
+WHERE first_name = 'Liam';
+
+-- Composite Index
+-- Total money paid by user_id 1 
+EXPLAIN ANALYZE
+SELECT SUM(o.total_paid), u.user_id FROM users u
+JOIN orders o ON u.user_id = o.user_id
+WHERE o.user_id = 1 AND o.product_ordered = 1
+GROUP BY user_id;
+
+CREATE INDEX user_id_product_ordered_type ON orders (`user_id`, `product_ordered`);
+
+EXPLAIN ANALYZE
+SELECT SUM(o.total_paid), u.user_id FROM users u
+JOIN orders o ON u.user_id = o.user_id
+WHERE o.user_id = 1 AND o.product_ordered = 1
+GROUP BY user_id;
+
+-- Index merge
+EXPLAIN 
+SELECT SUM(o.total_paid), u.user_id FROM users u
+JOIN orders o ON u.user_id = o.user_id
+WHERE o.user_id = 1 OR o.product_ordered = 1
+GROUP BY user_id;
+
